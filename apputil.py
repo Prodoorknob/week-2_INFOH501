9db97e0fd8e6356b14185284e2c8492db18f9425
Rajashekar Reddy Vedire <55252270+Prodoorknob@users.noreply.github.com>
2025-09-03 19:29:07 -0400

    Update apputil.py
    Added the required code for week 2 coding excercise

--- apputil.py
+++ apputil.py
@@ -2,12 +2,33 @@ import numpy as np
 
 
 # update/add code below ...
-
-def ways(n):
-    return None
+def ways(n, coin_types=[1,5,10]):
+    pennies=0
+    nickels=0
+    check=int(n/5)
+    collect=[]
+    if check !=0:
+        for i in range(0,check+1):
+            nickels=i
+            pennies=n-(i*5)
+            collect.append((pennies,nickels))
+    else:
+        nickels=0
+        pennies=n
+        collect.append((pennies,nickels))
+    return len(collect)
 
 def lowest_score(names, scores):
-    return None
+    data={n:s for n,s in zip(names, scores)}
+    lowest=min(data.values())
+    name=[k for k,v in data.items() if v==lowest]
+    return name[0]
 
 def sort_names(names, scores):
-    return None
+    data= {n:s for n, s in zip(names, scores)}
+    sorted_scores=np.sort(scores)[::-1]
+    sorted_data=[]
+    for i in sorted_scores:
+        name=[k for k,v in data.items() if v==i]
+        sorted_data.append((name[0],i))
+    return sorted_data
