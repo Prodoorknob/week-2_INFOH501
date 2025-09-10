f511da0d13daafcfcd39589d78e720c1614d20a7
Rajashekar Reddy Vedire <55252270+Prodoorknob@users.noreply.github.com>
2025-09-09 23:00:47 -0400

    Update apputil.py
    Added comments and test cases

--- apputil.py
+++ apputil.py
@@ -3,32 +3,52 @@ import numpy as np
 
 # update/add code below ...
 def ways(n, coin_types=[1,5,10]):
+     """
+    Returns the number of ways you can make change for a given amount of cents n
+    Examples: function   --->      (num pennies, num nickels)
+    """
     pennies=0
     nickels=0
-    check=int(n/5)
+    check=int(n/5)                             # using the remainder of change to distribute the nickels
     collect=[]
-    if check !=0:
-        for i in range(0,check+1):
+    if check !=0:                              # checking if there is at least one nickel possible
+        for i in range(0,check+1):             # iterating over the possible nickles to create different ways 
             nickels=i
-            pennies=n-(i*5)
+            pennies = n-(i*5)                  # subtracting the nickels to get the pennies possible
             collect.append((pennies,nickels))
-    else:
+    else:                                      # If no nickels are possible, the pennies are stored
         nickels=0
         pennies=n
         collect.append((pennies,nickels))
     return len(collect)
 
+
 def lowest_score(names, scores):
-    data={n:s for n,s in zip(names, scores)}
-    lowest=min(data.values())
-    name=[k for k,v in data.items() if v==lowest]
+    """
+    Returns the name of the student with the lowest score.
+    """
+    data={n:s for n,s in zip(names, scores)}  # Create a dictionary of names and scores
+    lowest=min(data.values())                 # Find the lowest score in the dictionary
+    name=[k for k,v in data.items() if v==lowest] # Locate the key for the lowest score
     return name[0]
 
+
 def sort_names(names, scores):
-    data= {n:s for n, s in zip(names, scores)}
-    sorted_scores=np.sort(scores)[::-1]
+    """
+    Returns the list of names of students in descending order of test score (i.e., a list of names, with associated scores in order from highest to lowest).
+    """
+    data= {n:s for n, s in zip(names, scores)} # Create a dictionary of names and scores
+    sorted_scores=np.sort(scores)[::-1] #sorting scores by descending order
     sorted_data=[]
     for i in sorted_scores:
-        name=[k for k,v in data.items() if v==i]
-        sorted_data.append(name[0])
+        name=[k for k,v in data.items() if v==i] # Find the name corresponding to the key from the sorted list
+        sorted_data.append(name[0])              
     return sorted_data
+
+
+#Test cases to check and verify the functions
+names = np.array(['Hannah', 'Astrid', 'Abdul', 'Mauve', 'Jung'])
+scores = np.array([99, 71, 85, 62, 91])
+Test_case_change=ways(12),ways(30), ways(3)
+Test_case_lowestscore= lowest_score(names,scores)
+Test_case_sort_names=sort_names(names,scores)
